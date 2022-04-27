52c1769c27fedf4b06e4efcdff650fe0e9ec3f45
Wongi Park <86551201+kalelpark@users.noreply.github.com>
2022-04-27 18:00:05 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-const backtoTop = document.getElementById('backtotop');
+const backtoTop = document.getElementById("backtotop");
+
 const checkScroll = () => {
-    
+    let pageYoffset = window.pageYOffset;
+
+    if (pageYoffset !== 0) {
+        backtoTop.classList.add('show');
+    } else {
+        backtoTop.classList.remove('show');
+    }
 }
+
+const moveBackToTop = () => {
+    if (window.pageYOffset > 0) {
+        window.scrollTo({top : 0, behavior : "smooth"})
+    }
+}
+
+window.addEventListener('scroll', checkScroll);
+backtoTop.addEventListener('click', moveBackToTop);
